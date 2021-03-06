6467e5b84147f0bd4f0754e8de849a380326c75f
Tania Tatula <tgrogol@gmail.com>
2021-03-06 18:16:50 +0200

    вирішує основне завдання

--- working.js
+++ working.js
@@ -1,24 +1,16 @@
 import images from './gallery-items.js';
 
 const selectedByIdGalleryEl = document.querySelector('.js-gallery');
+const openModal = document.querySelector('.js-lightbox');
+const closeModalBtn = document.querySelector('.lightbox__button');
+const bigImg = document.querySelector('.lightbox__content');
+const overlayEl = document.querySelector('.lightbox__overlay');
+const imgOflitebox = document.querySelector('.lightbox__image');
+const imgGallery = document.querySelector('.gallery__image')
+
 selectedByIdGalleryEl.insertAdjacentHTML('beforeend', createsListOfImg(images));
 
-
-
 function createsListOfImg (images) {
-// const liItems = images.reduce((imegeList, imag) => imegeList + `<li class="gallery__item">
-// <a
-//   class="gallery__link"
-//   href="${imag.original}"
-// >
-//   <img
-//     class="gallery__image"
-//     src="${imag.preview}"
-//     data-source="${imag.original}"
-//     alt="${imag.description}"
-//   />
-// </a>
-// </li>`, '');
 return images.map(imag => `<li class="gallery__item">
 <a
   class="gallery__link"
@@ -34,3 +26,41 @@ return images.map(imag => `<li class="gallery__item">
 </li>`).join('');
 
 };
+
+selectedByIdGalleryEl.addEventListener('click', onOpenBigImages);
+closeModalBtn.addEventListener('click', onCloseBigImages);
+overlayEl.addEventListener('click', onCloseBigImages);
+
+function onOpenBigImages(evt) {
+  evt.preventDefault();
+
+  if(!evt.target.classList.contains('gallery__image')){
+    return;
+  }
+  openModal.classList.add('is-open');
+
+
+  const imgAlt = evt.target.getAttribute("alt");
+  const imgBigSrc = evt.target.dataset.source;
+  
+  
+
+
+  openModal.classList.add('is-open');
+  
+
+  if(openModal.closest('.is-open')){
+    imgOflitebox.alt = imgAlt;
+    imgOflitebox.src = imgBigSrc;
+  }  
+}
+
+
+
+function onCloseBigImages(evt) { 
+  openModal.classList.remove('is-open');
+  imgOflitebox.alt = '';
+  imgOflitebox.src = '';
+}
+
+
